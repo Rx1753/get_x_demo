bec22d5f2ef4f764854e2e439d95966a41d537d8
Rx1753 <radixdt.1753@gmail.com>
2023-01-17 16:01:18 +0530

    Updated github actions

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,6 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(title: Obx(() => Text('Clicks :: ${c.count}'))),
       body: Center(
